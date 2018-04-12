33b46bee2afb6faa2b64bfa18a158e023c423cd9
darkeyes143 <36121515+darkeyes143@users.noreply.github.com>
2018-04-12 11:16:02 +0800

    Update content.php

--- content.php
+++ content.php
@@ -16,16 +16,16 @@
 
 
        <ul>
-           <li><a href="person 1.php" target="content">Load Person 1</a> </li>
-           <li><a href="person 2.php" target="content">Load Person 2</a> </li>
-           <li><a href="person 3.php" target="content">Load Person 3</a> </li>
-           <li><a href="person 4.php" target="content">Load Person 4</a> </li>
-           <li><a href="person 5.php" target="content">Load Person 5</a> </li>
-           <li><a href="person 6.php" target="content">Load Person 6</a> </li>
-           <li><a href="person 7.php" target="content">Load Person 7</a> </li>
-           <li><a href="person 8.php" target="content">Load Person 8</a> </li>
-           <li><a href="person 9.php" target="content">Load Person 9</a> </li>
-           <li><a href="person 10.php" target="content">Load Person 10</a> </li>
+           <li><a href="person1.php" target="content">Load Person 1</a> </li>
+           <li><a href="person2.php" target="content">Load Person 2</a> </li>
+           <li><a href="person3.php" target="content">Load Person 3</a> </li>
+           <li><a href="person4.php" target="content">Load Person 4</a> </li>
+           <li><a href="person5.php" target="content">Load Person 5</a> </li>
+           <li><a href="person6.php" target="content">Load Person 6</a> </li>
+           <li><a href="person7.php" target="content">Load Person 7</a> </li>
+           <li><a href="person8.php" target="content">Load Person 8</a> </li>
+           <li><a href="person9.php" target="content">Load Person 9</a> </li>
+           <li><a href="person10.php" target="content">Load Person 10</a> </li>
        </ul>
 
 <img src="https://s-i.huffpost.com/gen/813511/images/o-FRIENDSHIP-facebook.jpg" alt="friends" height="200px" width="600px">
