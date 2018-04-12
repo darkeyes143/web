bc583de324d82a9b9fade94a17c33decea5439e5
darkeyes143 <36121515+darkeyes143@users.noreply.github.com>
2018-04-12 11:18:17 +0800

    Update menu_2.php

--- menu_2.php
+++ menu_2.php
@@ -12,16 +12,16 @@
 </head>
 <body>
 <h3>Menu 2</h3>
-<p><a href="person 1.php" target="content">Person 1</a></p>
-<p><a href="person 2.php" target="content">Person 2</a></p>
-<p><a href="person 3.php" target="content">Person 3</a></p>
-<p><a href="person 4.php" target="content">Person 4</a></p>
-<p><a href="person 5.php" target="content">Person 5</a></p>
-<p><a href="person 6.php" target="content">Person 6</a></p>
-<p><a href="person 7.php" target="content">Person 7</a></p>
-<p><a href="person 8.php" target="content">Person 8</a></p>
-<p><a href="person 9.php" target="content">Person 9</a></p>
-<p><a href="person 10.php" target="content">Person 10</a></p>
+<p><a href="person1.php" target="content">Person 1</a></p>
+<p><a href="person2.php" target="content">Person 2</a></p>
+<p><a href="person3.php" target="content">Person 3</a></p>
+<p><a href="person4.php" target="content">Person 4</a></p>
+<p><a href="person5.php" target="content">Person 5</a></p>
+<p><a href="person6.php" target="content">Person 6</a></p>
+<p><a href="person7.php" target="content">Person 7</a></p>
+<p><a href="person8.php" target="content">Person 8</a></p>
+<p><a href="person9.php" target="content">Person 9</a></p>
+<p><a href="person10.php" target="content">Person 10</a></p>
 
 </body>
 </html>
